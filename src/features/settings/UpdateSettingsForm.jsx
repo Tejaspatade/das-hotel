d94fcbecdba8f06c165b8da6bc74634744ed4038
Tejas Patade <tej.patade69@gmail.com>
2023-07-10 12:07:19 +0530

    Settings Fetching & Updating from Supabase

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,74 +1,73 @@
-import Spinner from 'ui/Spinner';
-import { useSettings } from 'features/settings/useSettings';
-import { useUpdateSetting } from 'features/settings/useUpdateSetting';
+import { useSettings } from "./useSettings";
+import { useUpdateSetting } from "./useUpdateSettings";
 
-import Form from 'ui/Form';
-import FormRow from 'ui/FormRow';
-import Input from 'ui/Input';
+import Spinner from "../../components/Spinner";
+import Form from "../../components/Form";
+import FormRow from "../../components/FormRow";
+import Input from "../../components/Input";
 
 function UpdateSettingsForm() {
-  const {
-    settings: {
-      minBookingLength,
-      maxBookingLength,
-      maxGuestsPerBooking,
-      breakfastPrice,
-    } = {},
-    isLoading,
-  } = useSettings();
-  const { mutate: updateSetting, isLoading: isUpdating } = useUpdateSetting();
+	const {
+		settings: {
+			minBookingLength,
+			maxBookingLength,
+			maxGuestsPerBooking,
+			breakfastPrice,
+		} = {},
+		isLoading,
+	} = useSettings();
+	const { updateSetting, isUpdating } = useUpdateSetting();
 
-  // return <Spinner />;
-  if (isLoading) return <Spinner />;
+	if (isLoading) return <Spinner />;
 
-  function handleBlur(e, field) {
-    const { value } = e.target;
+	function handleBlur(e, field) {
+		const { value } = e.target;
 
-    if (!value) return;
-    updateSetting({ [field]: value });
-  }
+		if (!value) return;
+		updateSetting({ [field]: value });
+	}
 
-  // This time we are using UNCONTROLLED fields, so we will NOT store state
-  return (
-    <Form>
-      <FormRow label='Minimum nights/booking'>
-        <Input
-          type='number'
-          defaultValue={minBookingLength}
-          onBlur={(e) => handleBlur(e, 'minBookingLength')}
-          disabled={isUpdating}
-          id='min-nights'
-        />
-      </FormRow>
-      <FormRow label='Maximum nights/booking'>
-        <Input
-          type='number'
-          defaultValue={maxBookingLength}
-          onBlur={(e) => handleBlur(e, 'maxBookingLength')}
-          disabled={isUpdating}
-          id='max-nights'
-        />
-      </FormRow>
-      <FormRow label='Maximum guests/booking'>
-        <Input
-          type='number'
-          defaultValue={maxGuestsPerBooking}
-          onBlur={(e) => handleBlur(e, 'maxGuestsPerBooking')}
-          disabled={isUpdating}
-          id='max-guests'
-        />
-      </FormRow>
-      <FormRow label='Breakfast price'>
-        <Input
-          type='number'
-          defaultValue={breakfastPrice}
-          onBlur={(e) => handleBlur(e, 'breakfastPrice')}
-          disabled={isUpdating}
-          id='breakfast-price'
-        />
-      </FormRow>
-    </Form>
-  );
+	// This time we are using UNCONTROLLED fields, so we will NOT store state
+	return (
+		<Form>
+			<FormRow label="Minimum nights/booking">
+				<Input
+					type="number"
+					defaultValue={minBookingLength}
+					onBlur={(e) => handleBlur(e, "minBookingLength")}
+					disabled={isUpdating}
+					id="min-nights"
+				/>
+			</FormRow>
+			<FormRow label="Maximum nights/booking">
+				<Input
+					type="number"
+					defaultValue={maxBookingLength}
+					onBlur={(e) => handleBlur(e, "maxBookingLength")}
+					disabled={isUpdating}
+					id="max-nights"
+				/>
+			</FormRow>
+			<FormRow label="Maximum guests/booking">
+				<Input
+					type="number"
+					defaultValue={maxGuestsPerBooking}
+					onBlur={(e) => handleBlur(e, "maxGuestsPerBooking")}
+					disabled={isUpdating}
+					id="max-guests"
+				/>
+			</FormRow>
+			<FormRow label="Breakfast price">
+				<Input
+					type="number"
+					defaultValue={breakfastPrice}
+					onBlur={(e) => handleBlur(e, "breakfastPrice")}
+					disabled={isUpdating}
+					id="breakfast-price"
+				/>
+			</FormRow>
+		</Form>
+	);
 }
 
 export default UpdateSettingsForm;
