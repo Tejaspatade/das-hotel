f35534c2f6ab365c859e9d243b1e69d12c178ab3
Tejas Patade <tej.patade69@gmail.com>
2023-07-14 07:45:27 +0530

    Pagination Bug

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -43,6 +43,7 @@ const Filter = ({ filterField, options }) => {
 	// Handler Functions
 	const handleClick = (value) => {
 		searchParams.set(filterField, value);
+		if (searchParams.get("page")) searchParams.set("page", 1);
 		setSearchParams(searchParams);
 	};
 
