360d044191c3fcecbb30b3f47c19433e0131efaa
Tejas Patade <tej.patade69@gmail.com>
2023-07-18 08:59:47 +0530

    Light & Dark Mode

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import Checkin from "./pages/Checkin";
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./components/AppLayout";
 import ProtectedRoute from "./components/ProtectedRoute";
+import { DarkModeProvider } from "./contexts/DarkModeContext";
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -29,58 +30,60 @@ const queryClient = new QueryClient({
 
 const App = () => {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<ReactQueryDevtools initialIsOpen={false} />
-			<GlobalStyles />
-			<BrowserRouter>
-				<Routes>
-					<Route
-						element={
-							<ProtectedRoute>
-								<AppLayout />
-							</ProtectedRoute>
-						}
-					>
+		<DarkModeProvider>
+			<QueryClientProvider client={queryClient}>
+				<ReactQueryDevtools initialIsOpen={false} />
+				<GlobalStyles />
+				<BrowserRouter>
+					<Routes>
 						<Route
-							index
-							element={<Navigate replace to="dashboard" />}
-						/>
-						<Route path="dashboard" element={<Dashboard />} />
-						<Route path="bookings" element={<Bookings />} />
-						<Route
-							path="bookings/:bookingId"
-							element={<Booking />}
-						/>
-						<Route
-							path="checkin/:bookingId"
-							element={<Checkin />}
-						/>
-						<Route path="cabins" element={<Cabins />} />
-						<Route path="account" element={<Account />} />
-						<Route path="settings" element={<Settings />} />
-						<Route path="users" element={<Users />} />
-					</Route>
-					<Route path="login" element={<Login />} />
-					<Route path="*" element={<PageNotFound />} />
-				</Routes>
-			</BrowserRouter>
-			<Toaster
-				position="top-center"
-				gutter={12}
-				containerStyle={{ margin: "8px" }}
-				toastOptions={{
-					success: { duration: 3000 },
-					error: { duration: 6000 },
-					style: {
-						fontSize: "16px",
-						maxWidth: "500px",
-						padding: "16px 12px",
-						backgroundColor: "var(--color-grey-0)",
-						color: "var(--color-grey-800)",
-					},
-				}}
-			/>
-		</QueryClientProvider>
+							element={
+								<ProtectedRoute>
+									<AppLayout />
+								</ProtectedRoute>
+							}
+						>
+							<Route
+								index
+								element={<Navigate replace to="dashboard" />}
+							/>
+							<Route path="dashboard" element={<Dashboard />} />
+							<Route path="bookings" element={<Bookings />} />
+							<Route
+								path="bookings/:bookingId"
+								element={<Booking />}
+							/>
+							<Route
+								path="checkin/:bookingId"
+								element={<Checkin />}
+							/>
+							<Route path="cabins" element={<Cabins />} />
+							<Route path="account" element={<Account />} />
+							<Route path="settings" element={<Settings />} />
+							<Route path="users" element={<Users />} />
+						</Route>
+						<Route path="login" element={<Login />} />
+						<Route path="*" element={<PageNotFound />} />
+					</Routes>
+				</BrowserRouter>
+				<Toaster
+					position="top-center"
+					gutter={12}
+					containerStyle={{ margin: "8px" }}
+					toastOptions={{
+						success: { duration: 3000 },
+						error: { duration: 6000 },
+						style: {
+							fontSize: "16px",
+							maxWidth: "500px",
+							padding: "16px 12px",
+							backgroundColor: "var(--color-grey-0)",
+							color: "var(--color-grey-800)",
+						},
+					}}
+				/>
+			</QueryClientProvider>
+		</DarkModeProvider>
 	);
 };
 
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,20 +1,27 @@
 import styled from "styled-components";
+import { useDarkMode } from "../contexts/DarkModeContext";
 
 const StyledLogo = styled.div`
-  text-align: center;
+	text-align: center;
 `;
 
 const Img = styled.img`
-  height: 9.6rem;
-  width: auto;
+	height: 9.6rem;
+	width: auto;
 `;
 
 function Logo() {
-  return (
-    <StyledLogo>
-      <Img src="/logo-light.png" alt="Logo" />
-    </StyledLogo>
-  );
+	// Consuming context from provider
+	const { isDarkMode } = useDarkMode();
+
+	return (
+		<StyledLogo>
+			<Img
+				src={`/logo-${isDarkMode ? "dark" : "light"}.png`}
+				alt="Logo"
+			/>
+		</StyledLogo>
+	);
 }
 
 export default Logo;
